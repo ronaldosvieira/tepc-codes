3e3130473d6d9fdbca63f23bfec1643fae266d8b
Ronaldo Vieira <ronaldo.vieira@ufrrj.br>
2016-09-18 11:10:05 -0300

    Create soma-divcon.c

---
+++ soma/soma-divcon.c
@@ -0,0 +1,142 @@
+/*
+ ============================================================================
+ Name        : soma-divcon.c
+ Author      : Ronaldo Vieira
+ Version     : 1.0.0
+ Copyright   : MIT License
+ Description : Soma paralela de números de uma lista utilizando divisão e conquista
+ ============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "mpi.h"
+
+#define DEBUG 1
+#define printflush(s, ...) do {if (DEBUG) {printf(s, ##__VA_ARGS__); fflush(stdout);}} while (0)
+
+int* get_random_nums(int amount) {
+    int *rand_nums = (int *) malloc(sizeof(int) * amount);
+    int i;
+
+    for (i = 0; i < amount; i++) {
+        rand_nums[i] = rand() % 10;
+    }
+
+    return rand_nums;
+}
+
+int recSum(int *rand_nums, int amount, int p, int my_rank, int parent_rank) {
+	printflush("%d: sumRec with %d-sized array and p = %d\n", my_rank, amount, p);
+	int sum, child_rank, new_amount, new_p;
+
+	if (p == 1 /*|| amount <= 2*/) {
+		int i;
+		sum = 0;
+
+		for (i = 0; i < amount; i++) {
+			sum += rand_nums[i];
+		}
+
+		printflush("%d: Partial sum: %d\n", my_rank, sum);
+	} else {
+		child_rank = ceil(my_rank + (p / 2.0));
+		new_amount = (int) amount / 2;
+		new_p = (int) p / 2;
+
+		MPI_Send(&new_amount, 1, MPI_INT, child_rank, 2, MPI_COMM_WORLD);
+		MPI_Send(&new_p, 1, MPI_INT, child_rank, 3, MPI_COMM_WORLD);
+		MPI_Send(rand_nums + (amount - new_amount), new_amount, MPI_INT, child_rank, 0, MPI_COMM_WORLD);
+
+		printflush("%d: Sent to %d\n", my_rank, child_rank);
+
+		int sum1 = recSum(rand_nums, amount - new_amount, p - new_p, my_rank, my_rank);
+		int sum2;
+		MPI_Status status;
+
+		MPI_Recv(&sum2, 1, MPI_INT, child_rank, 1, MPI_COMM_WORLD, &status);
+
+		sum = sum1 + sum2;
+	}
+
+	if (parent_rank == my_rank) {
+		return sum;
+	} else {
+		MPI_Send(&sum, 1, MPI_INT, parent_rank, 1, MPI_COMM_WORLD);
+		printflush("%d: Sent sum to %d\n", my_rank, parent_rank);
+
+		return -1;
+	}
+}
+
+int main(int argc, char* argv[]){
+	srand(time(NULL));
+
+	int i;
+	int sum;
+	int *rand_nums = NULL; /* array of random numbers */
+	int my_rank; /* rank of process */
+	int p;       /* number of processes */
+	MPI_Status status;   /* return status for receive */
+
+	/* start up MPI */
+	
+	MPI_Init(&argc, &argv);
+	
+	/* find out process rank */
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+	
+	/* find out number of processes */
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+	if (argc != 2) {
+		if (my_rank == 0)
+			printf("Usage: %s (size of array >= 2*p)\n", argv[0]);
+
+		MPI_Finalize();
+		return -1;
+	}
+
+	int amount = atoi(argv[1]);
+
+	if (amount < 0) {
+		if (my_rank == 0)
+			printf("Error: Size of array must be greater than zero.\n");
+
+		MPI_Finalize();
+		return -1;
+	}
+
+	/* rank 0 creates and sends the array */
+	if (my_rank == 0) {
+		rand_nums = (int*) get_random_nums(amount);
+
+		printf("%d: Created the array: [", my_rank);
+		for (i = 0; i < amount - 1; i++) {
+			printf("%d, ", rand_nums[i]);
+		}
+		printflush("%d]\n", rand_nums[amount - 1]);
+
+		sum = recSum(rand_nums, amount, p, my_rank, my_rank);
+
+        printflush("%d: End. Sum is %d\n", my_rank, sum);
+	/* other ranks receive the array from rank 0 */
+	} else {
+		MPI_Recv(&amount, 1, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+		rand_nums = (int *) malloc(sizeof(int) * amount);
+
+		MPI_Recv(&p, 1, MPI_INT, status.MPI_SOURCE, 3, MPI_COMM_WORLD, &status);
+		MPI_Recv(rand_nums, amount, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+		printflush("%d: Received %d-sized array from %d \n", my_rank, amount, status.MPI_SOURCE);
+
+		recSum(rand_nums, amount, p, my_rank, status.MPI_SOURCE);
+	}
+
+	free(rand_nums);
+
+	/* shut down MPI */
+	MPI_Finalize(); 
+	
+	return 0;
+}
